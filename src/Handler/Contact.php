4e6e94c43c7b70d6d229a699ce7149d6633c64ed
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-09-28 14:32:27 +0300

    Added post function for contact form

--- src/Handler/Contact.php
+++ src/Handler/Contact.php
@@ -6,6 +6,11 @@ class Contact
 {
     public function execute(): void
     {
-        require_once __DIR__ . '/../../templates/contact.phtml';
+        require_once dirname(__FILE__, 3). '/templates/contact.html';
+    }
+
+    public function store(array $request): void
+    {
+        echo 'Name is ' . $request['name'];
     }
 }
